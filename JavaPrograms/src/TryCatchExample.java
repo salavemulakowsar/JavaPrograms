032311bc227ec16bb0a3d3bd098b7ce5d4a67f4b
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-06 21:30:12 +0530

    trycatchexample

---
+++ JavaPrograms/src/TryCatchExample.java
@@ -0,0 +1,19 @@
+
+public class TryCatchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 try  
+	        {  
+	        int data=50/0;    
+	        }  
+	              
+	        catch(ArithmeticException e)  
+	        {  
+	            System.out.println(e);  
+	        }  
+	        System.out.println("rest of the code");  
+
+	}
+
+}
