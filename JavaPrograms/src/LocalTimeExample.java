8ac45780dd2cb42e0b9ead71106b70c61aad28ef
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-06 20:36:48 +0530

    localtime

---
+++ JavaPrograms/src/LocalTimeExample.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+public class LocalTimeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    LocalTime time = LocalTime.now();  
+	    System.out.println(time); 
+	}
+
+}
