170340468d800f9ae9c3d7201bad2e4903ce6b76
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-08 11:29:53 +0530

    Lifebeforefuncinterface

---
+++ JavaPrograms/src/lambdaexpresions/BeforeFuncInterface.java
@@ -0,0 +1,10 @@
+package lambdaexpresions;
+
+public class BeforeFuncInterface {
+	public static void main(String[] args)
+	{
+	ImpISum i=new ImpISum();
+	System.out.println(i.add(2,3));
+	}
+
+}
---
+++ JavaPrograms/src/lambdaexpresions/ImpISum.java
@@ -0,0 +1,10 @@
+package lambdaexpresions;
+
+public class ImpISum implements ISum {
+	@Override
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+
+}
