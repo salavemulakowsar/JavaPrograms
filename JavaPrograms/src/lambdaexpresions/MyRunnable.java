afe5b31d6595ddc3ca228520acf3b73a9e894559
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-08 11:57:58 +0530

    lambdathreads second commit

--- JavaPrograms/src/lambdaexpresions/MyRunnable.java
+++ JavaPrograms/src/lambdaexpresions/MyRunnable.java
@@ -2,7 +2,16 @@ package lambdaexpresions;
 
 public class MyRunnable implements Runnable {
 	public void run() {
+		for(int i=0;i<5;i++)
+		{
 		System.out.println("In the thread");
 	}
+	
+	//()->
+	for(int i=0;i<5;i++)
+	{
+		System.out.println("in the thread");
+	}
 
+}
 }
--- JavaPrograms/src/lambdaexpresions/ThreadTest.java
+++ JavaPrograms/src/lambdaexpresions/ThreadTest.java
@@ -3,11 +3,21 @@ package lambdaexpresions;
 public class ThreadTest {
 	public static void main(String[] args)
 	{
-		MyRunnable r=new MyRunnable();
+		//MyRunnable r=new MyRunnable();
+		//Thread t=new Thread(r);
+		//t.start();
+		Runnable r=()->{
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("child thread");
+			}
+		};
 		Thread t=new Thread(r);
 		t.start();
 		for(int i=0;i<5;i++)
+		{
 			System.out.println("mainthread");
+	}
 	}
 
 }
