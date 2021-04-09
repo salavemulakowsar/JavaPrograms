269cc604a2ec981a8c9f488a8090a623848732ad
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-09 10:47:32 +0530

    predicateempsal commit

---
+++ JavaPrograms/src/InBuilt/FunctionalInterface/Employee.java
@@ -0,0 +1,13 @@
+package InBuilt.FunctionalInterface;
+
+public class Employee {
+	public Employee(String name, int salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	String name;
+	int salary;
+	
+
+}
--- JavaPrograms/src/InBuilt/FunctionalInterface/TestPredicate.java
+++ JavaPrograms/src/InBuilt/FunctionalInterface/TestPredicate.java
@@ -1,13 +1,31 @@
 package InBuilt.FunctionalInterface;
 
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class TestPredicate {
-
+     static public ArrayList<Employee> employees;
 	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
-       Predicate<Integer> p = i -> i % 2 == 0;
-       System.out.println(p.test(10));
+		/*
+		 * Predicate<Integer> p = i -> i % 2 == 0; System.out.println(p.test(10));
+		 */
+		populateEmployees();
+		Predicate<Employee> p1 = (e) -> e.salary>=10000;
+		for(Employee e : employees)
+		{
+			if(p1.test(e)) {
+				System.out.println(e.name);
+			}
+		}
+	}  
+
+	private static void populateEmployees() {
+		// TODO Auto-generated method stub
+		employees = new ArrayList<Employee>();
+		employees.add(new Employee("name",2355));
+		employees.add(new Employee("john",11000));
 	}
 
 }
