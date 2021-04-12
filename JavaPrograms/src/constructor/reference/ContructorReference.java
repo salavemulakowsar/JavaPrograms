e03ad461587231bcf67f97d2d6f50273ca356632
kowsar <kowsar@LAPTOP-EOR4URSO>
2021-04-12 11:44:24 +0530

    constructorreference secondcommit

--- JavaPrograms/src/constructor/reference/ContructorReference.java
+++ JavaPrograms/src/constructor/reference/ContructorReference.java
@@ -4,12 +4,17 @@ class Sample{
 	{
 		System.out.println("i am in sample constructor");
 	}
+	public Sample(String name)
+	{
+		System.out.println("name:"+name);
+	}
 }
 public class ContructorReference {
 
 	public static void main(String[] args) {
 		ISample i =  Sample::new;
-		Sample s1 = i.get();
+		//Sample s1 = i.get();
+		Sample s1 = i.get("kowsar");
 
 	}
 
--- JavaPrograms/src/constructor/reference/ISample.java
+++ JavaPrograms/src/constructor/reference/ISample.java
@@ -1,6 +1,7 @@
 package constructor.reference;
 
 public interface ISample{
-	public Sample get();
+	//public Sample get();
+	public Sample get(String name);
 
 }
